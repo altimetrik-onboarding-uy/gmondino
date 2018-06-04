407000e2a5b86e2f08fe291efec1541781f16755
Gianni Mondino <gmondino@altimetrik.com>
2018-06-04 16:53:26 -0300

    error found with the creation of contact

--- Bloggist/src/aura/PostCreation/PostCreationController.js
+++ Bloggist/src/aura/PostCreation/PostCreationController.js
@@ -1,11 +1,31 @@
 ({
 	clickCreatePost : function(component, event, helper) {
-		 var validPost = component.find('postform').reduce(function (valid, inputCmp){
+		var validPost = component.find('postform').reduce(function (valid, inputCmp){
             inputCmp.showHelpMessageIfInvalid();
             return valid && inputCmp.get('v.validity').valid;
         }, true);
         if(validPost) {
-            helper.createPost(component, component.get("v.newPost"));
+            var action = component.get("c.authorExist");
+            var authorName = component.get("v.authorName");
+            action.setParams({"authorName": authorName});
+            console.log("parameter done");
+            action.setCallback(this, function(response){
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    console.log("callBackSuccess");
+                    console.log(response.getReturnValue());
+                    if (!response.getReturnValue()) {
+                        if(confirm('You don\'t have an associated contact with that author name do you wish to create one?')) {
+                     		console.log("beforeHelper");
+                            helper.createPost(component, component.get("v.newPost"));       
+                        }
+                    } else {
+                        helper.createPost(component, component.get("v.newPost"));       
+                    }
+            	}
+            })
+            $A.enqueueAction(action);
+            console.log("actionEnqueued");
         }
 	}
 })
--- Bloggist/src/aura/PostCreation/PostCreationHelper.js
+++ Bloggist/src/aura/PostCreation/PostCreationHelper.js
@@ -1,23 +1,32 @@
 ({
 	createPost : function(component,post) {
-		var action = component.get("c.savePost");
-        action.setParams({
-            "post": post
-        });
+        var action = component.get("c.savePost");
+        var authorName = component.get("v.authorName");
+        action.setParams({ "post": post, 
+                          "authorName": authorName});
         action.setCallback(this, function(response){
-            var state = response.getState();
+        	var state = response.getState();
             if (state === "SUCCESS") {
-                var createPostEvent = $A.get("e.c:createPostEvent");
-                createPostEvent.setParams({
-	            "createdPost": response.getReturnValue() })
-                createPostEvent.fire();
-                component.set("v.newPost", { 'sobjectType': 'Post__c',
-                        'Title__c': '',
-                        'Status__c': 'Draft',
-                        'Tag__c': '',
-                        'Content__c': ''})
+                console.log("successfullResponsed");
+                console.log(response.getReturnValue());
+           		var createPostEvent = $A.get("e.c:createPostEvent");
+            	createPostEvent.setParams({"createdPost": response.getReturnValue()})
+            	createPostEvent.fire();
+            	component.set("v.newPost", { 'sobjectType': 'Post__c',
+               	   	  'Title__c': '',
+               	   	  'Status__c': 'Draft',
+               	   	  'Tag__c': '',
+               	   	  'Content__c': ''})
+                component.set("v.authorName", '');
+            } else if (state === 'ERROR'){
+                var errors = response.getError();
+                for(var i=0; i<= errors.length; i++){
+                    console.log(errors[i]);
+                }
             }
+            	
         });
-        $A.enqueueAction(action);
-	}
+       	$A.enqueueAction(action);
+        console.log("equeuedFiring");
+    }
 })
